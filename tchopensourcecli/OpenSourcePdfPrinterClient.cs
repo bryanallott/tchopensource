960d8333e75470c67a49a38c6c39f11c12006dc4
Bryan Allott <bryan@theconvergency.co.za>
2020-08-12 15:20:30 +0200

    api call to stitch pdfs example included

--- tchopensourcecli/OpenSourcePdfPrinterClient.cs
+++ tchopensourcecli/OpenSourcePdfPrinterClient.cs
@@ -15,6 +15,15 @@ namespace tchopensourcecli
             return Execute(request);
         }
 
+        public byte[] StitchPdfs(byte[] file1, byte[] file2)
+        {
+            RestSharp.RestRequest request = new RestSharp.RestRequest("/PdfUtils/Stitch", RestSharp.Method.POST);
+            request.AddHeader("content-type", "multipart/form-data");
+            request.AddFile("firstFile", file1, "first.pdf");
+            request.AddFile("secondFile", file2, "second.pdf");
+            return Execute(request);
+        }
+
         //https://stackoverflow.com/questions/11513607/restsharp-unable-to-cast-object-of-system-string-to-type-system-generic-idicti
         /*In that code path the library attempts to assign your value to an IDictionary<string,object> regardless of what type it is.*/
         public byte[] Execute(RestSharp.RestRequest request)
--- tchopensourcecli/Program.cs
+++ tchopensourcecli/Program.cs
@@ -11,6 +11,18 @@ namespace tchopensourcecli
         {
             return GetFile("affidavit.html");
         }
+        private static byte[] GetFileBytes(string resource)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            using (Stream resFilestream = assembly.GetManifestResourceStream($"tchopensourcecli.resources.{resource}"))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    resFilestream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
         private static string GetFile(string resource)
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
@@ -39,6 +51,12 @@ namespace tchopensourcecli
             string htmlContent = GetAffidavit();
             byte[] affidavitPdf = printer.GetPdf(htmlContent);
             Console.WriteLine(SaveFile(affidavitPdf));
+
+            byte[] file1 = GetFileBytes("preface.pdf");
+            byte[] file2 = GetFileBytes("report.pdf");
+            byte[] sticthedPdf = printer.StitchPdfs(file1, file2);
+            Console.WriteLine(SaveFile(sticthedPdf));
+
         }
     }
 }
